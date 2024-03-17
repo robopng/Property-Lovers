846e5e8d6b46300a181d74de8ca6d4e348693814
robopng <109000271+robopng@users.noreply.github.com>
2024-03-17 15:47:54 -0500

    create program structure and assign initial tasks

---
+++ work/sim_render.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file sim_render.cpp
+ * @author Layla Payton tpayton1@luc.edu
+ * @brief Rendering and (static) display for the dating sim implemented with SFML
+ * @version 0.0
+ * @date 2024-03-17
+ * 
+ * TODO:
+ *  - Set up a class that can hold and statically render images 
+ *  - Set up subclasses for the background, NPC, dialog box, etc. Or, whatever
+ *    implementation of this that you prefer.
+ *  - Figure out how the art files should be stored in src to meet your needs here
+ */
---
+++ work/sim_runner.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file sim_runner.cpp
+ * @author Liam Wilson lwilson18@luc.edu
+ * @brief Master file of all parts of the dating sim (listener, renderer, and game logic)
+ * @version 0.0
+ * @date 2024-03-17
+ * 
+ * 
+ */
