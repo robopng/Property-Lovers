d18beb228f444a1ab61201d5e1bd8930a867d1f7
robopng <109000271+robopng@users.noreply.github.com>
2024-03-17 16:03:51 -0500

    restructuring and addition of menu/button files

---
+++ work/sim/sim_runner.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file sim_runner.cpp
+ * @author Liam Wilson lwilson18@luc.edu
+ * @brief Master file of all parts of the dating sim (listener, renderer, and game logic)
+ * @version 0.0
+ * @date 2024-03-17
+ * 
+ * TODO:
+ *  - Await dialog control and render control
+ *  - Setup listeners for player input to next dialog, buttons, etc
+ *  - Link menu elements for functionality (i.e. view past dialog, etc) (implemment first
+ *    with keybinds?)
+ *  - Setup sim with full functionality:
+ *     - Display everything
+ *     - Open date sequence
+ *     - Dialog box, dialog, dialog progression, dialog past view
+ *     - Player options, player option buttons
+ *     - Close date sequence, progression to next part of game
+ */
