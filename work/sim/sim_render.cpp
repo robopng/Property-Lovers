1ae312d22711541e7dc061d4affb406c45c0e0a8
paytongithub <trp31604@gmail.com>
2024-03-20 23:05:13 -0500

    sim_render written

--- work/sim/sim_render.cpp
+++ work/sim/sim_render.cpp
@@ -1,3 +1,9 @@
+#include <vector>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include "sim_render.hpp"
+
+using namespace sf;
 /**
  * @file sim_render.cpp
  * @author Layla Payton tpayton1@luc.edu
@@ -11,3 +17,40 @@
  *    implementation of this that you prefer.
  *  - Figure out how the art files should be stored in src to meet your needs here
  */
+
+struct {
+    Sprite sprite;
+    Window window;
+    RenderTarget& target;
+} str;
+
+//Just input 1 and 1 for scale and opacity for now
+SimRender::SimRender(int x, int y, std::string FileName, Window* window, double opacity, double scale) {
+    sf::Texture texture;
+    texture.loadFromFile(FileName);
+    str.sprite.setTexture(texture); 
+    setCoordinates(x,y);
+    setScale(1);
+}
+SimRender::~SimRender() {
+    //not neededd as of 3/20
+}
+
+void SimRender::render() {
+    str.target.draw(str.sprite, RenderStates::Default);
+}
+void SimRender::removeRender(){
+    SimRender::~SimRender();
+}
+void SimRender::setOpacity(double opacity) {
+    //not needed as of 3/20
+}
+void SimRender::setScale(double scale) {
+    str.sprite.setScale(scale, scale);
+}
+void SimRender::setCoordinates(int x, int y) {
+    str.sprite.setPosition(x,y);
+}
+void SimRender::setInvisible(bool isInvisible) {
+    //not needed as of 3/20
+}
