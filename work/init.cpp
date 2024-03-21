9c5cbb1388201b565e09109c514a525e19a2c19c
Manali Deb <132420274+ManaliDeb@users.noreply.github.com>
2024-03-21 11:08:49 -0500

    init.cpp

--- work/init.cpp
+++ work/init.cpp
@@ -1,6 +1,29 @@
 #include "sim_showrunner.hpp"
-#include <SFML/Window>
+#include <SFML/Graphics.hpp>
 
-int main(int argc, char* argv[]) { 
+using namespace sf;
 
+int main(int argc, char* argv[]) {
+
+  VideoMode vm(960, 540);
+  RenderWindow window(vm, "Window")
+
+  while(window.isOpen()){
+    if (Keyboard::isKeyPressed(Keyboard::Escape)){
+      window.close();
+    }
+      /*
+      * put stuff in here for
+      * updating scenes, drawing scenes, etc
+      */
+    //clear last frame
+    window.clear();
+    
+    //render graphics
+    
+    //render game scene
+    window.display();
+  }
+
+  return 0;
 }
